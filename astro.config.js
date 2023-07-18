89e6a392da1588de85d780899f1f14aebe0df2bf
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-07-18 17:46:02 -0500

    add commented adapter guidance

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+// Eventually, replace this import with the official one - we're using a
+// patched version for now.
+// import deno from "@astrojs/deno";
 import deno from "deno-astro-adapter";
 
 // https://astro.build/config

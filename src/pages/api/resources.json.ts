409de0e05ef5ff281651df0de368debb8d505af3
Ohrrkan <49190058+ohrrkan@users.noreply.github.com>
2023-09-25 11:13:39 +0200

    Update resources.json.ts

--- src/pages/api/resources.json.ts
+++ src/pages/api/resources.json.ts
@@ -2,7 +2,7 @@ import { APIRoute } from "astro";
 // To use Deno KV, swiitch this import to "resources_kv" in the same directory
 import { deleteResource } from "../../data/resources.ts";
 
-export const del: APIRoute = async ({ request }) => {
+export const DELETE: APIRoute = async ({ request }) => {
   const title = new URL(request.url).searchParams.get("title");
   if (!title) return new Response(null, { status: 400 });
 
